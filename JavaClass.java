067a083f759331886fa89b4a008028d3937e0c00
Atsushi Eno <atsushieno@veritas-vos-liberabit.com>
2011-08-11 18:34:34 +0900

    Field constant values must be "escaped".
    Differs at android.util.Patterns fields.

--- JavaClass.java
+++ JavaClass.java
@@ -149,7 +149,7 @@ public class JavaClass implements Comparable<JavaClass> {
 				else if (type == "boolean")
 					e.setAttribute ("value", field.getBoolean (null) ? "true" : "false");
 				else if (type == "java.lang.String")
-					e.setAttribute ("value", "\"" + ((String) field.get (null)) + "\"");
+					e.setAttribute ("value", "\"" + ((String) field.get (null)).replace ("\\", "\\\\") + "\"");
 			} catch (Exception exc) {
 				System.err.println ("Error accessing constant field " + field.getName () + " value for class " + getName ());
 			}
