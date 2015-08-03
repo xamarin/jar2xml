79922933372ddacac2517ace0f8364546349b136
Atsushi Eno <atsushieno@gmail.com>
2015-08-03 11:41:53 -0400

    Detecting @deprecated was too hacky.
    It was just checking "caution" section. It covered @deprecated but in few
    times it had false positives (negatives?) and other kind of "caution"s
    were treated as @deprecated.
    Now it also checks text content (still hacky, but seems to work).

--- scraper-collector.sh
+++ scraper-collector.sh
@@ -12,10 +12,10 @@ do
 	export len=`expr length "$docpath" + 1`
 	echo "<file name='${f:$len}'>"
 	echo "<fields>"
-	xmllint --html --xmlout $f 2>/dev/null | xmlstarlet sel -t -c "//p[@class='caution']/../../h4[@class='jd-details-title']/text()"
+	xmllint --html --xmlout $f 2>/dev/null | xmlstarlet sel -t -c "//p[@class='caution'][contains(., 'deprecated')]/../../h4[@class='jd-details-title']/text()"
 	echo "</fields>"
 	echo "<methods>"
-	xmllint --html --xmlout $f 2>/dev/null | xmlstarlet sel -t -c "//p[@class='caution']/ancestor::div[h4/@class='jd-details-title' and h4/span/@class='sympad']/h4/descendant::text()"
+	xmllint --html --xmlout $f 2>/dev/null | xmlstarlet sel -t -c "//p[@class='caution'][contains(., 'deprecated')]/ancestor::div[h4/@class='jd-details-title' and h4/span/@class='sympad']/h4/descendant::text()"
 	echo "</methods>"
 	echo "</file>"
 done
